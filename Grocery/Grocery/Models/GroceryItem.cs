e6aca08b201901e93d3427d51bb9235711885c01
andradacrisan-gep <112691959+andradacrisan-gep@users.noreply.github.com>
2024-03-25 19:41:17 +0200

    add grocery - basic crud

---
+++ Grocery/Grocery/Models/GroceryItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Grocery.Models
+{
+    public class GroceryItem
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public double ItemPrice { get; set; }
+        public bool Availability { get; set; }
+    }
+}
